f7f78d733d1aa96c7449531a7797ccbc7b34e5de
Andantei <zaoyinji@gmail.com>
2023-01-18 11:26:58 -0800

    improve exception handling, basic read apis

--- src/tuneflow_devkit/debugger.py
+++ src/tuneflow_devkit/debugger.py
@@ -1,7 +1,9 @@
 import socketio
 import uvicorn
-from tuneflow_py import TuneflowPlugin, Song
+from tuneflow_py import TuneflowPlugin, Song, LabelText, ReadAPIs
 from typing import Type
+from locale import getlocale
+import traceback
 
 
 class Debugger:
@@ -11,16 +13,21 @@ class Debugger:
         self._serialized_song: str | None = None
         self._plugin: TuneflowPlugin | None = None
         self._plugin_class = plugin_class
+        self._daw_sid: None | str = None
+        self._sio: None | socketio.AsyncServer = None
         self.port = 18818
 
     def start(self):
         # create a Socket.IO server
         sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+        self._sio = sio
 
         def handle_connect(sid, environ, auth):
+            self._daw_sid = sid
             print("new daw connection")
 
         def handle_disconnect(sid):
+            self._daw_sid = None
             print("daw disconnected")
 
         async def handle_set_song(sid, data):
@@ -40,9 +47,12 @@ class Debugger:
                 return {"status": "SONG_OR_PLUGIN_NOT_READY"}
             song = Song.deserialize(self._serialized_song)
             try:
-                self._plugin = self._plugin_class.create(song)
+                self._plugin = self._plugin_class.create(
+                    song, self.create_read_apis())
             except Exception as e:
-                print("Init plugin exception:", e)
+                print("================ Run Plugin Exception ================")
+                traceback.print_exc()
+                print("======================================================")
                 return {
                     "status": "INIT_PLUGIN_EXCEPTION"
                 }
@@ -57,9 +67,11 @@ class Debugger:
             params = data["params"]
             song = Song.deserialize(self._serialized_song)
             try:
-                self._plugin.run(song, params)
+                self._plugin.run(song, params, self.create_read_apis())
             except Exception as e:
-                print("Run plugin exception:", e)
+                print("================ Run Plugin Exception ================")
+                traceback.print_exc()
+                print("======================================================")
                 return {
                     "status": "RUN_PLUGIN_EXCEPTION"
                 }
@@ -79,3 +91,45 @@ class Debugger:
         # Wrap with ASGI application
         self._app = socketio.ASGIApp(self._sio)
         uvicorn.run(self._app, host='127.0.0.1', port=self.port)
+
+    @staticmethod
+    def get_system_locale():
+        return getlocale()
+
+    def create_read_apis(self) -> ReadAPIs:
+        def translate_label(label_text: LabelText):
+            if type(label_text) is str:
+                return label_text
+
+            current_locale = Debugger.get_system_locale()[0]
+            match_locales = [item.split(
+                '-')[0].lower() for item in label_text.keys() if item == current_locale]
+            match_locale = match_locales[0] if len(match_locales) > 0 else None
+
+            if match_locale is not None:
+                return label_text[match_locale]  # type: ignore
+            elif len(label_text.keys()) > 0:  # type: ignore
+                return label_text[label_text.keys()[0]]  # type: ignore
+            else:
+                return ''
+
+        def serialize_song(song: Song):
+            return song.serialize()
+
+        def deserialize_song(encoded_song: str):
+            return Song.deserialize(encoded_song)
+
+        async def get_available_audio_plugins():
+            response = None
+
+            def handle_get_audio_plugins_callback(sid, data):
+                response = data
+            await self._sio.emit(event='getAvailableAudioPlugins', data=None, to=self._daw_sid, callback=handle_get_audio_plugins_callback)
+            return response
+
+        return {
+            "translate_label": translate_label,
+            "serialize_song": serialize_song,
+            "deserialize_song": deserialize_song,
+            "get_available_audio_plugins": get_available_audio_plugins
+        }
